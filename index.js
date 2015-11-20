417dd21f45d2a6179232d5a350c17ce33787a1ee
Robert Grummt <robert@templex.net>
2015-11-20 15:23:10 +0000

    Third attempt

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+    var fs = require('fs')
     response.send(fs.readFileSync('index.html').toString())
 })
 
