32b0aacd92613f54ece1cb9de559ba28e596e393
Robert Grummt <robert.grummt@templex.net>
2016-02-12 15:18:40 +0100

    Quick check for certin HTML tags using SSJS

---
+++ grader.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+*/
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile)
+{
+  var instr = infile.toString();
+  if(!fs.existsSync(instr))
+  {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1)
+  }
+};
+
+var cheerioHtmlFile = function(htmlfile)
+{
+  return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile)
+{
+  return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile)
+{
+  $ = cheerioHtmlFile(htmlfile.toString());
+  var checks = loadChecks(checksfile.toString()).sort();
+  var out = {};
+  for(var ii in checks)
+  {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+};
+
+var clone = function(fn)
+{
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
+};
+
+if(require.main == module)
+{
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file>', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+    .parse(process.argv);
+  var checkJson = checkHtmlFile(program.file, program.checks);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+}
+else
+{
+  exports.checkHtmlFile = checkHtmlFile;
+}
